1a488ca3e3add958cf3fe2b2755e57ff74534cda
unscalable <149246855+unscalable@users.noreply.github.com>
2024-01-26 16:59:28 +0200

    config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@
 -------------------
 Config = {}
 
-Config.Doctor = 0 -- Minimum Amount of EMS to work
+Config.Doctor = 1 -- Minimum Amount of EMS to work
 Config.Price = 100
 Config.ReviveTime = 20  --in msec
 
